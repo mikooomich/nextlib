b4aff30e96df393a7c9a9b81aa884c65a371ddfa
mikooomich <mikooomichael@gmail.com>
2025-03-06 02:05:09 -0500

    bump ndk, agp

--- media3ext/build.gradle.kts
+++ media3ext/build.gradle.kts
@@ -25,7 +25,7 @@ android {
             abiFilters += listOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
         }
 
-        ndkVersion = "25.2.9519653"
+        ndkVersion = "27.0.11718014"
     }
 
     compileOptions {
